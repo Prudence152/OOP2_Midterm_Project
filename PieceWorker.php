a781b94de82bd4dc6d49ffc44c9d57973c2592e4
Inoc Catherine J. <128596774+Prudence152@users.noreply.github.com>
2024-11-08 19:47:24 +0800

    Update PieceWorker.php

--- PieceWorker.php
+++ PieceWorker.php
@@ -1,41 +1,22 @@
 <?php
-
 require_once 'Employee.php';
 
-
-
 class PieceWorker extends Employee {
+    private $numberItems;
+    private $wagePerItem;
 
-    private int $itemsProduced;
-
-    private float $wagePerItem;
-
-
-
-    public function __construct($name, $address, $age, $companyName, $itemsProduced, $wagePerItem) {
-
+    public function __construct($name, $address, $age, $companyName, $numberItems, $wagePerItem) {
         parent::__construct($name, $address, $age, $companyName);
-
-        $this->itemsProduced = $itemsProduced;
-
+        $this->numberItems = $numberItems;
         $this->wagePerItem = $wagePerItem;
-
     }
 
-
-
-    public function earnings(): float {
-
-        return $this->itemsProduced * $this->wagePerItem;
-
+    public function earnings() {
+        return $this->numberItems * $this->wagePerItem;
     }
 
-
-
-    public function __toString(): string {
-
-        return parent::__toString() . ", Items Produced: {$this->itemsProduced}, Wage per Item: {$this->wagePerItem}";
-
+    public function toString() {
+        return parent::toString() . ", Number of Items: $this->numberItems, Wage per Item: $this->wagePerItem";
     }
-
 }
+?>
