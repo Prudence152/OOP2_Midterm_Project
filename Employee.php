6e486661aa8a1d4f494a1b60e5f6f0e7fd9c94dc
Inoc Catherine J. <128596774+Prudence152@users.noreply.github.com>
2024-11-08 19:50:25 +0800

    Update Employee.php

--- Employee.php
+++ Employee.php
@@ -1,33 +1,26 @@
 <?php
-
 require_once 'Person.php';
 
-
-
 abstract class Employee extends Person {
-
-    private string $companyName;
-
-
+    private $companyName;
 
     public function __construct($name, $address, $age, $companyName) {
-
         parent::__construct($name, $address, $age);
-
         $this->companyName = $companyName;
-
     }
 
+    public function getCompanyName() {
+        return $this->companyName;
+    }
 
+    public function setCompanyName($companyName) {
+        $this->companyName = $companyName;
+    }
 
     abstract public function earnings();
 
-
-
-    public function __toString(): string {
-
-        return parent::__toString() . ", Company: {$this->companyName}";
-
+    public function toString() {
+        return parent::toString() . ", Company: $this->companyName";
     }
-
 }
+?>
